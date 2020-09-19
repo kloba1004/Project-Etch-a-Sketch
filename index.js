1f00e91f83c77fa46a97d91f6e9cbd3be3cf3c00
kloba1004 <ante.bebic@gmail.com>
2020-09-20 00:18:13 +0200

    Completed the project. Also did the darkening the color one just for the sake of checking out if I know how to do it, but I will push to origin only the random color version.

---
+++ index.js
@@ -0,0 +1,36 @@
+document.body.setAttribute('style', 'margin: 0;');
+
+let container = document.createElement("div");
+document.body.appendChild(container);
+
+function createGrid(squareNumber) {
+    container.setAttribute('style', `width: 600px; height: 600px; margin: 100px auto 0; border: black 10px solid; display: grid; grid-template-rows: repeat(${squareNumber}, 1fr); grid-template-columns: repeat(${squareNumber}, 1fr);`);
+
+    for (i=1; i<= squareNumber*squareNumber; i++) {
+        let newSquareDiv = document.createElement('div');
+        newSquareDiv.classList.add('colorChange');
+        container.appendChild(newSquareDiv);
+    }
+
+    let colorChange = document.querySelectorAll('.colorChange');
+    colorChange.forEach( (child) => {
+        child.addEventListener('mouseenter', () => {
+            let red = Math.ceil(Math.random()*100),
+                green = Math.ceil(Math.random()*100),
+                blue = Math.ceil(Math.random()*100);
+            child.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
+        });
+    });
+}
+
+createGrid(15);
+
+let button = document.querySelector('.button');
+button.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let pencilSize = (prompt('Choose a number of squares between 2 and 64:'));
+    if (pencilSize>64 || pencilSize<2) return;
+    createGrid(pencilSize);
+})
